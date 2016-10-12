772813c02b476000074b2177c373e6effd0cf517
radityasurya <radityasurya@hotmail.com>
2016-10-12 02:56:35 +0200

    Add unit test week6

---
+++ Week6/Facade/FacadeTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Facade;
+using Facade.subsystem;
+using System.Collections.Generic;
+
+namespace FacadeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TravelFacade travel = new TravelFacade();
+            List<object> results = new List<object>();
+
+            DateTime from = DateTime.Now;
+            DateTime to = from.AddDays(3);
+
+            results = travel.getFlightsAndHotels(from, to);
+
+            // test the stub if it is making flights and hotels data
+            Assert.AreEqual(true, results.Count > 0);
+
+            // Check if the results is list of hotels
+            Assert.IsInstanceOfType(results[0], typeof(List<Hotel>));
+
+            // Check if the resutls is list of flights
+            Assert.IsInstanceOfType(results[1], typeof(List<Flight>));
+
+        }
+    }
+}
